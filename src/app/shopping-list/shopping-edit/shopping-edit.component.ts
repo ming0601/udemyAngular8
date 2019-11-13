d16977717b4722d7c7ce32adb1d2ca604d2fe5cb
ming0601 <fangming0601@gmail.com>
2019-11-13 23:03:55 +0100

    [#88-89] Add new Ingredient func in shopping-edit.component

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
+import { Ingredient } from './../../shared/ingredient.model';
+import { Component, OnInit, ViewChild, Output, EventEmitter, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', {static: false}) ingredientName: ElementRef;
+  @ViewChild('amountInput', {static: false}) ingredientAmount: ElementRef;
+  @Output() newAddedIngredient = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    this.newAddedIngredient.emit(
+      new Ingredient(this.ingredientName.nativeElement.value,
+                    this.ingredientAmount.nativeElement.value));
+  }
 }
