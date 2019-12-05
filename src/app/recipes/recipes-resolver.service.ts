3dd81e27b40b72195c6c5bf909183630ba0ebe4f
ming0601 <fangming0601@gmail.com>
2019-12-05 22:43:57 +0100

    [#284] Fixing update Recipes bug

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -1,3 +1,4 @@
+import { RecipeService } from './recipe.service';
 import { Recipe } from './recipe.model';
 import { DataStorageService } from './../shared/data-storage-service';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
@@ -6,9 +7,15 @@ import { Injectable } from '@angular/core';
 @Injectable({providedIn: 'root'})
 export class RecipeResolverService implements Resolve<Recipe[]> {
 
-    constructor(private dataStorageService: DataStorageService) {}
+    constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService) {}
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        return this.dataStorageService.fetchRecipes();
+        const recipes = this.recipeService.getRecipes();
+
+        if (recipes.length === 0) {
+            return this.dataStorageService.fetchRecipes();
+        } else {
+            return recipes;
+        }
     }
 }
