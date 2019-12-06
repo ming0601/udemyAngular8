d00354e06e38537d75228f3d05cf4ae6888df36d
ming0601 <fangming0601@gmail.com>
2019-12-06 12:31:03 +0100

    [#289] Created AuthForm

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,3 +1,4 @@
+import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  isLoginMode = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onChangeMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(authForm: NgForm) {
+    console.log(authForm.value);
   }
 
 }
