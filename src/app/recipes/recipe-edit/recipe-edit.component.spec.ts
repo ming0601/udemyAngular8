ae6856b8a31a486a850fa91ebc26ab417701f97b
ming0601 <fangming0601@gmail.com>
2019-12-26 11:23:23 +0100

    [#Tests] RecipeEdit added imports and providers and create component success

--- src/app/recipes/recipe-edit/recipe-edit.component.spec.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.spec.ts
@@ -1,14 +1,32 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { RecipeService } from './../recipe.service';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RecipeEditComponent } from './recipe-edit.component';
+import { MockRecipeService } from '../recipe-detail/recipe-detail.component.spec';
+import { Router, ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 
-describe('RecipeEditComponent', () => {
+fdescribe('RecipeEditComponent', () => {
   let component: RecipeEditComponent;
   let fixture: ComponentFixture<RecipeEditComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RecipeEditComponent ]
+      declarations: [ RecipeEditComponent ],
+      imports: [ReactiveFormsModule],
+      providers: [
+        // { provide: RecipeService, useValue: recipeServiceSpy },
+        { provide: RecipeService, useClass: MockRecipeService },
+        { provide: Router,
+          useClass: class {
+              navigate = jasmine.createSpy('navigate');
+          }
+        },
+        { provide: ActivatedRoute, useValue: { params: of({ id: 1 }) } },
+        ShoppingListService // service used for the MockRecipeService
+      ]
     })
     .compileComponents();
   }));
@@ -19,7 +37,7 @@ describe('RecipeEditComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  fit('should create', () => {
     expect(component).toBeTruthy();
   });
 });
