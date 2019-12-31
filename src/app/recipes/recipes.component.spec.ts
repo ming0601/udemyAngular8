b34b2dab43747596c516997edcd54e3b8f642181
ming0601 <fangming0601@gmail.com>
2019-12-31 14:53:32 +0100

    [#Tests] Finished recipes.component

--- src/app/recipes/recipes.component.spec.ts
+++ src/app/recipes/recipes.component.spec.ts
@@ -1,3 +1,8 @@
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
+import { RecipeService } from './recipe.service';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RouterTestingModule } from '@angular/router/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RecipesComponent } from './recipes.component';
@@ -8,7 +13,9 @@ describe('RecipesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RecipesComponent ]
+      imports: [RouterTestingModule.withRoutes([])],
+      declarations: [ RecipesComponent, RecipeListComponent, RecipeItemComponent ],
+      providers: [RecipeService, ShoppingListService]
     })
     .compileComponents();
   }));
