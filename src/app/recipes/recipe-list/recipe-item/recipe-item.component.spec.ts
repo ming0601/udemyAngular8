29b1c73dc6941d8bdcb6f3a39a6857139de96b11
ming0601 <fangming0601@gmail.com>
2019-12-31 12:08:38 +0100

    [#Tests] Finished recipe-item.component

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.spec.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.spec.ts
@@ -1,6 +1,10 @@
+import { RouterTestingModule } from '@angular/router/testing';
+import { LoadingSpinnerComponent } from './../../../shared/loading-spinner/loading-spinner.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RecipeItemComponent } from './recipe-item.component';
+import { Recipe } from '../../recipe.model';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 describe('RecipeItemComponent', () => {
   let component: RecipeItemComponent;
@@ -8,7 +12,8 @@ describe('RecipeItemComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RecipeItemComponent ]
+      imports: [RouterTestingModule.withRoutes([])],
+      declarations: [ RecipeItemComponent, LoadingSpinnerComponent ]
     })
     .compileComponents();
   }));
@@ -16,10 +21,19 @@ describe('RecipeItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RecipeItemComponent);
     component = fixture.componentInstance;
+    component.recipeItem = new Recipe('test-name', 'test-description', 'test-imagePath', [new Ingredient('test-ingredient-name', 2)]);
+    component.index = 0;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should feed HTML with preset values', () => {
+    const dom = fixture.debugElement.nativeElement;
+    expect(dom.querySelector('h4').textContent).toContain('test-name');
+    expect(dom.querySelector('p').textContent).toContain('test-description');
+    expect(dom.querySelector('img').alt).toContain('test-name');
   });
 });
